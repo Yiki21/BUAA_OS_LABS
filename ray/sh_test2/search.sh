0b777908c2dcbc4b27b4eeccc320a67d8d0a95c1
root <root@Yi.>
2025-02-26 20:40:20 +0800

    niubi

--- ray/sh_test2/search.sh
+++ ray/sh_test2/search.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 #First you can use grep (-n) to find the number of lines of string.
 #Then you can use awk to separate the answer.
+
+file=$1
+str=$2
+dirfile=$3
+
+sed -n "/${str}/=" "${file}" > "$dirfile"
