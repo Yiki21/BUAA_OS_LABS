a19f0cb59a229ae5ed72b02cd8f00ad1530ad374
root <root@Yi.>
2025-02-27 17:46:52 +0800

    relax

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
-int main() {
-	int n;
-	scanf("%d", &n);
 
-	if () {
-		printf("Y\n");
-	} else {
-		printf("N\n");
-	}
-	return 0;
+int is_palindrome(int num) {
+    int digit[5], j = 0, i = 0;
+    while (num) {
+        digit[j++] = num % 10;
+        num /= 10;
+    }
+    j--;
+    for (; i <= j; i++, j--) {
+        if (digit[i] != digit[j])
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (is_palindrome(n)) {
+        printf("Y\n");
+    } else {
+        printf("N\n");
+    }
 }
