c559eb7ba227961d4cc73a93a0d62dd831bccc2c
张义 <23371544@buaa.edu.cn>
2025-03-13 20:07:55 +0800

    dosrp

--- exam_1.sh
+++ exam_1.sh
@@ -22,12 +22,12 @@ done
 
 
 gcc -o result/verify result/code/0.c result/code/1.c result/code/2.c result/code/3.c \
-result/code/4 result/code/5.c result/code/6.c result/code/7.c result/code/8.c result/code/9.c \
+result/code/4.c result/code/5.c result/code/6.c result/code/7.c result/code/8.c result/code/9.c \
 result/code/10.c result/code/11.c result/code/12.c result/code/13.c result/code/14.c \
 result/code/15.c result/code/16.c result/code/17.c result/code/18.c result/code/19.c result/code/20.c 
 
 ./result/verify 2> stderr.txt
 
-chmod stderr.txt 440
+chmod 440 stderr.txt
 
 
