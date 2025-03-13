1045bfe0270eec98b3d56f69016b34da4849f934
张义 <23371544@buaa.edu.cn>
2025-03-13 20:13:46 +0800

    diaud

--- exam_1.sh
+++ exam_1.sh
@@ -20,7 +20,6 @@ do
 	((i++))
 done
 
-
 gcc -o result/verify result/code/0.c result/code/1.c result/code/2.c result/code/3.c \
 result/code/4.c result/code/5.c result/code/6.c result/code/7.c result/code/8.c result/code/9.c \
 result/code/10.c result/code/11.c result/code/12.c result/code/13.c result/code/14.c \
