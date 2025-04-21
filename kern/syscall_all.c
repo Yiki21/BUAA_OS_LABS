ed6bbfc711357925303225aaa9ff7dedc42b1a89
ZhangYiUser <Yiki@YiKi.>
2025-04-21 20:04:36 +0800

    feuioho

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -438,7 +438,7 @@ int sys_ipc_try_send(u_int envid, u_int value, u_int srcva, u_int perm) {
 	/* Return -E_INVAL if 'srcva' is not zero and not mapped in 'curenv'. */
 	if (srcva != 0) {
 		/* Exercise 4.8: Your code here. (8/8) */
-		if ((p = page_lookup(curenv->env_pgdir, curenv->env_asid, srcva)) == NULL) {
+		if ((p = page_lookup(curenv->env_pgdir, srcva, NULL)) == NULL) {
 			return -E_INVAL;
 		}
 		try(page_insert(e->env_pgdir, e->env_asid, p, e->env_ipc_dstva, perm));
