5083bc790d23286dbf63e387344c8b96e239eb81
ZhangYi <13887325901@163.com>
2025-05-07 10:45:06 +0800

    first

--- init/init.c
+++ init/init.c
@@ -50,11 +50,11 @@ void mips_init(u_int argc, char **argv, char **penv, u_int ram_low_size) {
     env_init();
 
     // lab3:
-    ENV_CREATE_PRIORITY(user_bare_loop, 1);
-    ENV_CREATE_PRIORITY(user_bare_loop, 2);
+    //ENV_CREATE_PRIORITY(user_bare_loop, 1);
+    //ENV_CREATE_PRIORITY(user_bare_loop, 2);
 
     // lab4:
-    ENV_CREATE(user_tltest);
+    //ENV_CREATE(user_tltest);
     //ENV_CREATE(user_fktest);
     //ENV_CREATE(user_pingpong);
 
@@ -62,9 +62,9 @@ void mips_init(u_int argc, char **argv, char **penv, u_int ram_low_size) {
     // ENV_CREATE(user_icode);  // This must be the first env!
 
     // lab5:
-    // ENV_CREATE(user_fstest);
-    // ENV_CREATE(fs_serv);  // This must be the second env!
-    // ENV_CREATE(user_devtst);
+    ENV_CREATE(user_fstest);
+    ENV_CREATE(fs_serv);  // This must be the second env!
+    //ENV_CREATE(user_devtst);
 
     // lab3:
     schedule(1);
